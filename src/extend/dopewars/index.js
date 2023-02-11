5500d604b3af3daa45d080d0f8471ad135e0dd92
Ryuzaki01 <wagmiarmy.dev@gmail.com>
2023-02-12 01:21:55 +0700

    + Fix dopewars

--- src/extend/dopewars/index.js
+++ src/extend/dopewars/index.js
@@ -1,5 +1,5 @@
 export const extend = async (_chainId, metadata) => {
-  const Clothes = metadata.attributes.find(t => /Clothes/i.test(t.trait_type));
+  const Clothes = metadata.attributes.find(t => /Clothes/i.test(t.key));
   if (/None/i.test(Clothes.value)) {
     return null;
   }
