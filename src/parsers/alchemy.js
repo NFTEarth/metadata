c7cfc6a06242000b0e576c1514978715c647fb2e
Ryuzaki01 <wagmiarmy.dev@gmail.com>
2023-03-10 10:36:31 +0700

    + Metadata Image

--- src/parsers/alchemy.js
+++ src/parsers/alchemy.js
@@ -9,7 +9,7 @@ export const parse = (asset) => {
     // Token descriptions are a waste of space for most collections we deal with
     // so by default we ignore them (this behaviour can be overridden if needed).
     description: asset.description,
-    imageUrl: asset.metadata?.image,
+    imageUrl: asset.media?.[0]?.thumbnail || asset.metadata?.image,
     mediaUrl: asset.metadata?.animation_url || asset.media?.[0]?.gateway,
     attributes: (asset.metadata?.attributes || []).map((trait) => ({
       key: trait.trait_type,
